f46be3b5033c924d909af973b3fef01d89f7f785
Daniel_Xu <Daniel-Xu@users.noreply.github.com>
2018-05-22 20:10:27 +0800

    update melpa
    http://elpa.emacs-china.org/melpa/

--- cask-bootstrap.el
+++ cask-bootstrap.el
@@ -57,8 +57,8 @@
         (condition-case nil
             (mapc 'require cask-bootstrap-packages)
           (error
-           (add-to-list 'package-archives (cons "gnu" "https://elpa.gnu.org/packages/"))
-           (add-to-list 'package-archives (cons "melpa" "https://melpa.org/packages/"))
+           (add-to-list 'package-archives (cons "gnu" "http://elpa.emacs-china.org/gnu/"))
+           (add-to-list 'package-archives (cons "melpa" "http://elpa.emacs-china.org/melpa/"))
            (package-refresh-contents)
            (mapc
             (lambda (package)
